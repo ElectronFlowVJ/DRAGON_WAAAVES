52eda113556ad2ab45331802fa33e480fceb1ab4
Kayla Coyote <kayla@kilo.cc>
2026-02-01 21:10:39 -0800

    Strip EOL whitespace in src/*.

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 int main() {
     ofGLFWWindowSettings settings;
     settings.setGLVersion(4, 6);
-    
+
     // GUI WINDOW
     settings.setSize(1280, 720);
     settings.setPosition(glm::vec2(0, 0));
@@ -14,7 +14,7 @@ int main() {
     settings.decorated = true;
     shared_ptr<ofAppBaseWindow> guiWindow = ofCreateWindow(settings);
     guiWindow->setWindowTitle("Gravity Waaaves - Control");
-    
+
     // OUTPUT WINDOW
     // For single monitor testing, offset to overlap or use smaller size
     settings.setSize(1280, 720);
@@ -23,14 +23,14 @@ int main() {
     settings.decorated = true;  // Set to true for testing, false for fullscreen output
     shared_ptr<ofAppBaseWindow> mainWindow = ofCreateWindow(settings);
     mainWindow->setWindowTitle("Gravity Waaaves - Output");
-    
+
     // Create and link apps
     shared_ptr<ofApp> mainApp(new ofApp);
     shared_ptr<GuiApp> guiApp(new GuiApp);
     mainApp->gui = guiApp;
     guiApp->mainApp = mainApp.get();
     mainApp->mainWindow = mainWindow;
-    
+
     ofRunApp(guiWindow, guiApp);
     ofRunApp(mainWindow, mainApp);
     ofRunMainLoop();
